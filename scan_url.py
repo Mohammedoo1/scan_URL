c1faacdcd1cc64d5b750a9b9ead0b96398f6b0de
Mohammed Khaled <m6407308@gmail.com>
2025-11-30 13:53:28 +0300

    Update scan_url.py

--- scan_url.py
+++ scan_url.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import vt
-
+import requests as re
 API = "f201bfdb4e779f527e33ef7c05749409292f1976fcfa59899a3383cb02f00754"
 
 st.title("welcome in  Scan URL ")
@@ -64,6 +64,7 @@ def scan(URL):
 
 if st.button("Click me to start scanning"):
     scan(URL)
+
 
 
 
