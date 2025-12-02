6dab61f62a11d50d3b6c7ed58bbc371acdb4f4be
Mohammed Khaled <m6407308@gmail.com>
2025-12-01 22:35:37 -0800

    Update scan_url.py

--- scan_url.py
+++ scan_url.py
@@ -2,127 +2,116 @@ import streamlit as st
 import vt
 import requests as rq
 
-tab1, tab2 = st.tabs(["فحص روابط", "فك الروابط المختصرة"])
+tab1, tab2 = st.tabs(["  Scan URL  ", "  فك الروابط المختصرة  "])
 
 API_KEY = st.secrets["API_google"]
 API = st.secrets["API_virus"]
 
-st.title(" Scan URL ")
+with tab1:
+    st.title(" Scan URL ")
 
-URL = st.text_input("enter your URl :")
-client = vt.Client(API)
+    URL = st.text_input("enter your URl :")
+    danger_words = [
+        "malicious",
+        "phishing",
+        "malware",
+        "trojan",
+        "harmful",
+        "suspicious",
+        "spam",
+        "dangerous",
+    ]
 
-danger_words = [
-    "malicious",
-    "phishing",
-    "malware",
-    "trojan",
-    "harmful",
-    "suspicious",
-    "spam",
-    "dangerous",
-]
-
-
-def scan_g(URL):
-    try:
-        data = {
-            "threatInfo": {
-                "threatTypes": ["MALWARE", "SOCIAL_ENGINEERING"],
-                "platformTypes": ["ANY_PLATFORM"],
-                "threatEntryTypes": ["URL"],
-                "threatEntries": [{"url": URL}]
+    def scan_g(URL):
+        try:
+            data = {
+                "threatInfo": {
+                    "threatTypes": ["MALWARE", "SOCIAL_ENGINEERING"],
+                    "platformTypes": ["ANY_PLATFORM"],
+                    "threatEntryTypes": ["URL"],
+                    "threatEntries": [{"url": URL}]
+                }
             }
-        }
+            with st.spinner("Scanning..."):
+                response = rq.post(
+                    f"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={API_KEY}",
+                    json=data
+                )
 
-        with st.spinner("Scanning..."):
-            response = rq.post(
-                f"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={API_KEY}",
-                json=data
-            )
+            result = response.json()
 
-        result = response.json()
-
-        if "matches" in result:
-            st.error("⚠ Google Safe Browsing: Dangerous")
-            return "dangerous"
-
-        else:
-            st.success("✔ Google Safe Browsing: Safe")
-            return "safe"
-
-    except Exception as e:
-        st.write(e)
-
-
-def scan(URL):
-    client = vt.Client(API)
-
-    tables = []
-    is_dangerous = False
-
-    try:
-
-        analysis = client.scan_url(URL)
-
-        with st.spinner("Scanning..."):
-
-            while True:
-                result = client.get_object(f"/analyses/{analysis.id}")
-                if result.status == "completed":
-                    break
-
-        st.write("Scan completed!")
-        for engine, details in result.results.items():
-            results = details['category'].lower()
-
-            is_engine_dangerous = False
-            for word in danger_words:
-                if word in results:
-                    is_engine_dangerous = True
-                    break
-
-            if is_engine_dangerous:
-                tables.append({"engine": engine, "Category": results, "status": "dangerous"})
-                is_dangerous = True
-
+            if "matches" in result:
+                st.error("⚠ Google Safe Browsing: Dangerous")
+                return "dangerous"
             else:
-                tables.append({"engine": engine, "Category": results, "status": "safe"})
+                st.success("✔ Google Safe Browsing: Safe")
+                return "safe"
 
-        if is_dangerous:
-            st.error("dangerous")
-            return "dangerous"
+        except Exception as e:
+            st.write(e)
 
-        else:
-            st.success("safe")
-            return "safe"
-        st.table(tables)
-    except Exception as e:
-        st.write(e)
+    def scan(URL):
+        client = vt.Client(API)
+        tables = []
+        is_dangerous = False
+
+        try:
+            analysis = client.scan_url(URL)
+
+            with st.spinner("Scanning..."):
+                while True:
+                    result = client.get_object(f"/analyses/{analysis.id}")
+                    if result.status == "completed":
+                        break
+
+            st.write("Scan completed!")
+            for engine, details in result.results.items():
+                results = details['category'].lower()
+                is_engine_dangerous = False
+                for word in danger_words:
+                    if word in results:
+                        is_engine_dangerous = True
+                        break
+
+                if is_engine_dangerous:
+                    tables.append({"engine": engine, "Category": results, "status": "dangerous"})
+                    is_dangerous = True
+                else:
+                    tables.append({"engine": engine, "Category": results, "status": "safe"})
+            st.table(tables)
+
+            if is_dangerous:
+                st.error("dangerous")
+                return "dangerous"
+            else:
+                st.success("safe")
+                return "safe"
 
 
-choose = st.radio("choose where you want to check your link :",
-                  ["🛡️ VirusTotal Scan", "🔍 Google Safe Browsing Scan", "Both (for deep scan)"])
+        except Exception as e:
+            st.write(e)
 
-if st.button("Click me to start scanning"):
-    if not URL:
-        st.warning("❌ Please enter a URL before scanning.")
-        st.stop()
+    choose = st.radio(
+        "choose where you want to check your link :",
+        ["🛡️ VirusTotal Scan", "🔍 Google Safe Browsing Scan", "Both (for deep scan)"]
+    )
 
-    if choose == "🛡️ VirusTotal Scan":
-        scan(URL)
-    elif choose == "🔍 Google Safe Browsing Scan":
-        scan_g(URL)
-    elif choose == "Both (for deep scan)":
-        col1, col2 = st.columns(2)
-        with col1:
-            st.subheader("🔍 Google Safe Browsing")
-            g = scan_g(URL)
-        with col2:
-            st.subheader("🛡️ VirusTotal Scan")
-            v = scan(URL)
-        if g != v:
-            st.warning("⚠ Maybe it is risky, don't open it ")
+    if st.button("Click me to start scanning"):
+        if not URL:
+            st.warning("❌ Please enter a URL before scanning.")
+            st.stop()
 
-
-
+        if choose == "🛡️ VirusTotal Scan":
+            scan(URL)
+        elif choose == "🔍 Google Safe Browsing Scan":
+            scan_g(URL)
+        elif choose == "Both (for deep scan)":
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader("🔍 Google Safe Browsing")
+                g = scan_g(URL)
+            with col2:
+                st.subheader("🛡️ VirusTotal Scan")
+                v = scan(URL)
+            if g != v:
+                st.warning("⚠ Maybe it is risky, don't open it ")
